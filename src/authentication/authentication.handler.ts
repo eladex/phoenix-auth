688789430f4dfb59b04d0c8ce30da2c3c8aa21c4
elad biran <eladex@gmail.com>
2020-06-25 11:40:51 +0300

    style(authentication.handler): rename vars & add docs

--- src/authentication/authentication.handler.ts
+++ src/authentication/authentication.handler.ts
@@ -8,11 +8,17 @@ const { Strategy: ShragaStrategy } = require('passport-shraga');
 const MINUTE = 60; // 60 seconds in minute
 const {
   minutesExpires,
-  token,
+  token: tokenCookie,
   shragaURL,
   secret
 } = config.authentication;
 
+/**
+ * initializes the passport strategy
+ * returns the `passport.initialize()` middleware which must be called 
+ * at the start of connect or express based apps.
+ * `app.use(initialize())`
+ */
 export function initialize() {
   passport.use(new ShragaStrategy({ shragaURL }, (profile: any, done: any) => {
     done(null, profile);
@@ -20,20 +26,32 @@ export function initialize() {
   return passport.initialize();
 }
 
+/**
+ * Middleware to handle the user after successful authentication.
+ * signs a token, responds with `set-cookie`and then redirects the 
+ * user.
+ * @param req express Request
+ * @param res express Response
+ */
 export function handleUser(req: Request, res: Response) {
+  // issued at and expires
   const iat = Date.now() / 1000;
   const exp = iat + minutesExpires * MINUTE;
 
   const { RelayState, ...user } = req.user as any;
-  const userToken: any = { ...user, iat, exp };
+  // generate the token
+  const token = jwt.sign({ ...user, iat, exp }, secret);
 
   const redirectTo = RelayState || config.clientEndpoint;
 
-  const signedToken = jwt.sign(userToken, secret);
-  res.cookie(token, signedToken);
+  res.cookie(tokenCookie, token);
   res.redirect(redirectTo);
 }
 
+/**
+ * Returns a middleware that uses the strategy to authenticate the user.
+ * should be used on routes to trigger a login and on the callback routes (idp callback)
+ */
 export function authenticate() {
   return passport.authenticate('shraga', { session: false });
 }
