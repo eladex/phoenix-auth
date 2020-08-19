37b1b574977ab3d9590c5f355b1570fc7bed2bff
elad biran <eladex@gmail.com>
2020-08-19 14:07:43 +0300

    chore(authentication): use authneticator in auth routes

--- src/authentication/authentication.router.ts
+++ src/authentication/authentication.router.ts
@@ -1,9 +1,11 @@
-import { Router } from 'express';
-import { authenticate, handleUser } from './authentication.handler';
+import { Router, Request, Response } from 'express';
+// import { authenticate, handleUser } from './authentication.handler';
+import authenticator from './shragaJWTAuthenticator';
 
 const router = Router();
 
-router.get('/login', authenticate());
-router.post('/callback', authenticate(), handleUser);
+router.get('/login', authenticator.authenticate());
+router.get('/user', authenticator.authenticate(), (req: Request, res: Response) => res.send(req.user));
+router.post('/callback', authenticator.authenticate({ failureRedirect: '/error' }));
 
 export default router;
